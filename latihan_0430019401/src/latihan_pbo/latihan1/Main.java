cbe24a8bc11e7a5190614b39b948164554e9cd49
Muhammad-Edi31 <muhammad.edi@polindra.ac.id>
2026-02-11 09:24:30 +0700

    praktikum 11/02/2026

--- latihan_0430019401/src/latihan_pbo/latihan1/Main.java
+++ latihan_0430019401/src/latihan_pbo/latihan1/Main.java
@@ -1,7 +1,58 @@
 package latihan_pbo.latihan1;
 
+// import java.util.Scanner;
+
+class Cafe{
+   String nama;
+   String alamat; 
+
+   Cafe(String nama, String alamat){
+    this.nama = nama;
+    this.alamat = alamat;
+    System.out.println(nama);
+    System.out.println(alamat);
+   }
+
+    // method
+   void buka(){
+    System.out.println("Cafe " + nama + " Sudah buka");
+   }
+
+   public String getNama() {
+    return nama;
+   }
+
+   public void setNama(String nama, String alamat) {
+    this.nama = nama;
+    this.alamat = alamat;
+   }
+
+   public String getAlamat() {
+    return alamat;
+   }
+
+   public void setAlamat(String alamat) {
+    this.alamat = alamat;
+   }
+
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Praktikum Pertama");
+
+        // Scanner input = new Scanner(System.in);
+        // Cafe cafe = new Cafe();
+
+        // System.out.println("Masukkan nama cafe: ");
+        // cafe.nama = input.nextLine();
+
+        // System.out.println("nama yang diinpiy adalah : " + cafe.nama);
+        // cafe.buka();
+        
+   
+        Cafe cafe2 = new Cafe("Janji Jiwa", "Indramayu");
+        cafe2.setNama("kenangan", "Cirebon");
+        cafe2.buka();
+
     }
 }
